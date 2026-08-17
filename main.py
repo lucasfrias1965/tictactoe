6ef12f6e54d161244457406fafefa107e2358626
Lucas Frias <lucas@lucass-mbp.lan>
2026-08-17 16:16:06 -0500

    Implemented start of BST

--- main.py
+++ main.py
@@ -2,7 +2,24 @@ from typing import final
 
 
 
+class TicTacToeResult:
+    def __init__(self, board) -> None:
+        self._board = []
 
+
+class BinaryNode:
+    def __init__(self) -> None:
+        self._left = None
+        self._right = None
+        self._entry = None
+
+
+class BinarySearchTree:
+    def __init__(self) -> None:
+        self._top = None
+
+    def add(self, new_node: BinaryNode):
+        ...
 @final
 class TicTacToe:
     """A class that models TicTacToe, and beats you every time"""
@@ -20,7 +37,7 @@ class TicTacToe:
         #okay, we are gonna win based off of the position but
         # instead of (sanely) using the finite solution, we are going to make a BST
         # of all potential positions.
-
+        ...
     def ascii_display(self):
         print("-"*9)
         for i in range(0,9):
@@ -28,6 +45,16 @@ class TicTacToe:
             if (i+1) % 3 == 0:
                 print("\n")
         print("-"*9)
+    def did_letter_win(self, letter: str, eval_board=None):
+        if eval_board is None:
+            eval_board = self._board
+        for i in range(0,3):
+            if "".join(eval_board[0+i:i+2]) == letter*3:
+                return True
+        for i in range(0,3):
+            if eval_board[i] + eval_board[i+3] + eval_board[i+6] == letter*3:
+                return True
+        return False
 
 if __name__ == "__main__":
     ttt = TicTacToe(human_starts=True)
